f6d6dbd2bb7c7e075c810b0fb9262a9fa442cf35
yuri295 <jin_astro0302@naver.com>
2024-02-03 15:43:56 +0900

    feat: login-error

--- inused-app/src/containers/auth/LoginForm.js
+++ inused-app/src/containers/auth/LoginForm.js
@@ -1,14 +1,20 @@
-import { changeField, initializeForm } from '../../modules/auth';
-import AuthForm from '../../components/auth/AuthForm';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { changeField, initializeForm, login } from '../../modules/auth'; // login 액션 import 추가
+import AuthForm from '../../components/auth/AuthForm';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
   }));
-  //인풋 변경 이벤트 핸들러
+
+  // 인풋 변경 이벤트 핸들러
   const onChange = (e) => {
     const { value, name } = e.target;
     dispatch(
@@ -20,16 +26,35 @@ const LoginForm = () => {
     );
   };
 
-  //폼 등록 이벤트 핸들러
+  // 폼 등록 이벤트 핸들러
   const onSubmit = (e) => {
     e.preventDefault();
-    //구현 예정
+    const { userid, userpw } = form;
+    //한 칸이라도 비어 있다면
+    if ([userid, userpw].includes('')) {
+      setError('빈 칸을 모두 입력하세요.');
+      return;
+    }
+    dispatch(login({ userid, userpw })); // login 액션 dispatch
   };
 
-  //컴포넌트가 처음 렌더링될 때 form을 초기화함
+  // 컴포넌트가 처음 렌더링될 때 form을 초기화함
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      setError('로그인 실패');
+      return;
+    }
+    if (auth) {
+      console.log('로그인 성공');
+      navigate('/main');
+    }
+  }, [auth, authError, navigate]);
 
   return (
     <AuthForm
@@ -37,6 +62,7 @@ const LoginForm = () => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
