ec79ae4865e31db2277a1ede8bff1f8bbf5c818d
yuri295 <jin_astro0302@naver.com>
2024-02-09 13:13:41 +0900

    feat: header

--- inused-app/src/components/common/Header.js
+++ inused-app/src/components/common/Header.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import { Icon } from '@iconify/react';
+import Responsive from './Responsive';
 
 const HeaderBlock = styled.div`
   position: fixed;
   width: 100%;
-
   background: ${palette.blue[0]};
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled(Responsive)`
   height: 4rem;
   display: flex;
   align-items: center;
@@ -17,11 +17,10 @@ const Wrapper = styled.div`
 
   .icon {
     color: ${palette.gray[0]};
-    width: 30px;
-    height: 30px;
+    width: 25px;
+    height: 25px;
     cursor: pointer;
-    margin-right: 2rem;
-    cursor: pointer;
+    display: flex;
 
     &:hover {
       color: ${palette.gray[1]};
@@ -30,12 +29,9 @@ const Wrapper = styled.div`
 `;
 
 const LogoImage = styled.img`
-  width: 180px;
-  margin-left: 2rem;
+  width: 150px;
   margin-right: 2rem;
-  @media screen and (max-width: 426px) {
-    display: none;
-  }
+  display: flex;
 `;
 
 const SearchBar = styled.input`
@@ -43,13 +39,18 @@ const SearchBar = styled.input`
   border-radius: 20px;
   padding-left: 2.5rem;
   padding-top: 0.3rem;
+  margin-left: 10rem;
   outline: none;
-  height: 40px;
-  font-size: 0.9rem;
-  margin-left: 5rem;
+  height: 35px;
+  font-size: 0.8rem;
   width: 20rem;
   box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
   font-family: 'Jua';
+  @media (max-width: 1000px) {
+    display: none;
+  }
+
+  //검색 아이콘
   background: url('https://api.iconify.design/ion/search.svg?color=%23aaa')
     no-repeat;
   background-size: 20px 20px;
