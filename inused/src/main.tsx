4c6ab58c2a251f08ef6626bc9a5f0a6e2396ea89
jinyuri <jin_astro0302@naver.com>
2024-02-12 23:53:23 +0900

    feat: add main file

---
+++ inused/src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div>hi</div>
+  </StrictMode>
+);
