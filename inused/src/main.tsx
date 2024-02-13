8bc1ad2b8a30190df69c9e32ae8d9686082cc17a
jinyuri <jin_astro0302@naver.com>
2024-02-14 02:44:01 +0900

    test

--- inused/src/main.tsx
+++ inused/src/main.tsx
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom/client';
 import { Router } from './router';
 import 'normalize.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <Router />
-    </StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<StrictMode></StrictMode>);
