6b666ca49096ede3fd6332b18951d3dfc1ca0e4d
jinyuri <jin_astro0302@naver.com>
2024-02-15 04:29:53 +0900

    feat(input): add input component

---
+++ src/components/atoms/Input.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Input = styled.input`
+    border-radius: 20px;
+    height: 45px;
+    font-size: 0.9rem;
+    width: 100%;
+    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+    text-align: 10px center;
+    padding-left: 0.5rem;
+`;
---
+++ src/constants/color.ts
@@ -0,0 +1,8 @@
+export const COLOR = {
+    blue: {
+        main: 'background:linear-gradient(135deg, #e7f0fd 0%,#accbee 100%',
+    },
+    pink: {
+        main: 'background:linear-gradient(135deg, #dad4ec 0%,#f3e7e9 100%)',
+    },
+};
