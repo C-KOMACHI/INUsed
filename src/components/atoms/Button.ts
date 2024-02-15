d54e4effe12e85ba7b6298191435517600049de7
jinyuri <jin_astro0302@naver.com>
2024-02-15 15:18:46 +0900

    feat(button): button 컴포넌트 추가

---
+++ src/components/atoms/Button.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+import { COLOR } from '@/constants';
+
+export const Button = styled.button`
+    border-radius: 4px;
+    font-size: 1rem;
+    padding: 0.25rem 1rem;
+    cursor: pointer;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    background: ${COLOR.blue.button};
+    border-radius: 20px;
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
+    width: 100%;
+    font-size: 1.125rem;
+`;
