ddc9e9a20e55e2bda70a15abdc6f275dcba32ce3
jinyuri <jin_astro0302@naver.com>
2024-02-15 19:32:48 +0900

    feat(footer): 하단 개발자 이름 추가

--- src/components/atoms/Footer.tsx
+++ src/components/atoms/Footer.tsx
@@ -1,12 +1,13 @@
 import type { FC } from 'react';
 import { Stack, Box, Button, Grid } from '@mui/material';
+import styled from '@emotion/styled';
 import { COLOR } from '@/constants';
 
 const style = {
     containers: {
         width: '350px',
-        alignItems: 'center',
-        padding: '0 0 0 2.7rem',
+        padding: '0.5rem 0 0 2.7rem',
+        display: 'flex',
     },
     button: {
         color: `${COLOR.gray.main}`,
@@ -14,21 +15,36 @@ const style = {
     },
 };
 
+const StyledFooter = styled.div`
+    p {
+        font-size: 0.9rem;
+        font-family: 'Jura';
+        color: ${COLOR.pink.footer};
+        padding-top: 3rem;
+        text-align: center;
+    }
+`;
+
 export const Footer: FC = () => {
     return (
-        <Box sx={style.containers}>
-            <Stack direction="row" spacing={15}>
-                <Grid item xs={6}>
-                    <Button size="small" sx={style.button}>
-                        비밀번호 찾기
-                    </Button>
-                </Grid>
-                <Grid item xs={6}>
-                    <Button size="small" sx={style.button}>
-                        회원 가입
-                    </Button>
-                </Grid>
-            </Stack>
-        </Box>
+        <StyledFooter>
+            <Box sx={style.containers}>
+                <Stack>
+                    <Stack direction="row" spacing={15}>
+                        <Grid item xs={6}>
+                            <Button size="small" sx={style.button}>
+                                비밀번호 찾기
+                            </Button>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button size="small" sx={style.button}>
+                                회원 가입
+                            </Button>
+                        </Grid>
+                    </Stack>
+                    <p>Product By C-Komachi</p>
+                </Stack>
+            </Box>
+        </StyledFooter>
     );
 };
