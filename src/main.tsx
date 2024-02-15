362d867b674c3b0f464bac2b79822a5dc694c0e8
jinyuri <jin_astro0302@naver.com>
2024-02-15 09:33:27 +0900

    build: path alias 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { CssBaseline } from '@mui/material';
+import { Stack } from '@/stackflow';
+
 import '@stackflow/plugin-basic-ui/index.css';
-import { Stack } from './stackflow';
 
-const App: React.FC = () => {
-    return (
-        <div>
-            안뇽
-            <Stack />
-        </div>
-    );
-};
-
-export default App;
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <CssBaseline />
+        <Stack />
+    </StrictMode>,
+);
