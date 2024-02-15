bce6cef1dd7e2bdabde91d1dd60048c7322b2720
jinyuri <jin_astro0302@naver.com>
2024-02-15 15:28:10 +0900

    feat(logo): logo 컴포넌트 추가

--- src/activities/Login.tsx
+++ src/activities/Login.tsx
@@ -1,10 +1,11 @@
 import type { ActivityComponentType } from '@stackflow/react';
 import { Suspense } from 'react';
-import { Input, Button } from '@/components/atoms';
+import { Input, Button, Logo } from '@/components/atoms';
 
 export const Login: ActivityComponentType = () => {
     return (
         <Suspense>
+            <Logo src="Logo.png" alt="logo" />
             <Input placeholder="이메일" />
             <Input placeholder="비밀번호" />
             <Button>로그인</Button>
