2e29d673254ed925bdac8885dd016d2e51f7ee0a
jinyuri <jin_astro0302@naver.com>
2024-02-16 01:53:41 +0900

    feat(register): register 컴포넌트 추가

--- src/components/templates/LoginTemplate.tsx
+++ src/components/templates/LoginTemplate.tsx
@@ -3,18 +3,6 @@ import { Stack } from '@mui/material';
 import { FormBox, AppScreen } from '@/components/organisms';
 import { Footer } from '@/components/atoms';
 
-const LoginTemplateBlock = styled.div`
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    top: 0;
-    right: 0;
-    background: ${COLOR.blue.main};
-    overflow: scroll;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
 export const LoginTemplate: FC = () => {
     return (
         <AppScreen center>
