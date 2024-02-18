21d02693d733c9a7a6c17f476611f31fda9ea253
jinyuri <jin_astro0302@naver.com>
2024-02-18 18:32:35 +0900

    feat(post-item): postItem 컴포넌트 추가

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -3,7 +3,7 @@ import { Typography, type TypographyTypeMap } from '@mui/material';
 
 const variant = {
     title: 'h5',
-    subtitle: 'subtitle1',
+    subtitle: 'subtitle2',
     body: 'body1',
 };
 
@@ -19,6 +19,7 @@ const style = {
     overflow: 'hidden',
     whiteSpace: 'nowrap',
     textOverflow: 'ellipsis',
+    fontFamily: 'Jua',
 };
 
 export const Text: FC<Props> = ({ children, type = 'body' }) => {
