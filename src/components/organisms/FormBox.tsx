8fb664532bbd0c92369a2592ca73895ecb0ba2ae
jinyuri <jin_astro0302@naver.com>
2024-02-20 15:22:51 +0900

    fix(button): button 컴포넌트 이름 변경

--- src/components/organisms/FormBox.tsx
+++ src/components/organisms/FormBox.tsx
@@ -1,8 +1,8 @@
 import type { FC } from 'react';
-import { Stack, Box, Button } from '@mui/material';
+import { Stack, Box, Button as AuthButton } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import CheckIcon from '@mui/icons-material/Check';
-import { Input, LoginButton, Logo } from '@/components/atoms';
+import { Input, Button, Logo } from '@/components/atoms';
 import { COLOR } from '@/constants';
 
 const style = {
@@ -34,21 +34,21 @@ export const FormBox: FC<Props> = ({ login, register }) => {
                     <>
                         <Stack direction="row" spacing={2}>
                             <Input placeholder="닉네임" small />
-                            <Button size="small" sx={style.button} variant="contained" endIcon={<CheckIcon />}>
+                            <AuthButton size="small" sx={style.button} variant="contained" endIcon={<CheckIcon />}>
                                 중복확인
-                            </Button>
+                            </AuthButton>
                         </Stack>
                         <Stack direction="row" spacing={2}>
                             <Input placeholder="학교 이메일" small />
-                            <Button size="small" sx={style.button} variant="contained" endIcon={<SendIcon />}>
+                            <AuthButton size="small" sx={style.button} variant="contained" endIcon={<SendIcon />}>
                                 인증하기
-                            </Button>
+                            </AuthButton>
                         </Stack>
                     </>
                 )}
                 {login && <Input placeholder="학교 이메일" />} <Input placeholder="비밀번호" />
                 {register && <Input placeholder="비밀번호 확인" />}
-                {login ? <LoginButton>로그인</LoginButton> : <LoginButton>회원가입</LoginButton>}
+                {login ? <Button>로그인</Button> : <Button>회원가입</Button>}
             </Stack>
         </Box>
     );
