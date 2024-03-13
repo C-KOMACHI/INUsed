676ebc9308bd856c918d33362863d910152410bb
jinyuri <jin_astro0302@naver.com>
2024-03-13 19:23:35 +0900

    refactor(post-bottom-menubar): 버튼 색상 변경

--- src/stackflow.ts
+++ src/stackflow.ts
@@ -18,7 +18,7 @@ export const { Stack, useFlow, activities } = stackflow({
         MyPage,
         Post,
     },
-    initialActivity: () => 'Login',
+    initialActivity: () => 'Post',
 });
 
 export type TypeActivities = typeof activities;
