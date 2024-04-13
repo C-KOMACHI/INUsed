3fcca70afb7aebd391f0b456fe80b1b1f1c9287e
Hayoung Kang <rkdgkdudsam@naver.com>
2024-04-13 16:23:07 +0900

    style(notice-template): 공지사항 조회 페이지 하단바 삭제

--- src/components/templates/NoticeTemplate.tsx
+++ src/components/templates/NoticeTemplate.tsx
@@ -9,7 +9,7 @@ const notices = [
 
 export const NoticeTemplate: FC = () => {
     return (
-        <AppScreen backIcon bottomNavigation header borderRadius title="공지사항">
+        <AppScreen backIcon header borderRadius title="공지사항">
             <PostList posts={notices} />
         </AppScreen>
     );
