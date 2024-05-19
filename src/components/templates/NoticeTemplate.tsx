57efcf834c2a3fc5c41825c9497281b9016f9b45
Hayoung Kang <rkdgkdudsam@naver.com>
2024-05-19 15:22:41 +0900

    feat: 공지 조회(전체 조회) api 연결

--- src/components/templates/NoticeTemplate.tsx
+++ src/components/templates/NoticeTemplate.tsx
@@ -1,13 +1,17 @@
 import type { FC } from 'react';
 import { AppScreen, PostList } from '@/components/organisms';
 
-const notices = [
-    { id: 1, title: '중고거래가 금지된 품목 공지', createdAt: '2023.11.27' },
-    { id: 2, title: '악성유저 처벌 공지', createdAt: '2023.11.26' },
-    { id: 3, title: '1.0.0 버전 패치내역', createdAt: '2023.11.25' },
-];
+type Notice = {
+    id: number;
+    title: string;
+    createdAt: string;
+}
 
-export const NoticeTemplate: FC = () => {
+interface Props {
+    notices: Notice[];
+}
+
+export const NoticeTemplate: FC<Props> = ({notices}) => {
     return (
         <AppScreen backIcon header borderRadius title="공지사항">
             <PostList posts={notices} />
